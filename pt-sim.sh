b02d35c88e295b67ee6843dbdaca5d2a32d9f042
Brandon Robinson <brandon.clark.robinson@gmail.com>
2025-04-29 13:21:46 -0700

    ALMOST there...

--- pt-sim.sh
+++ pt-sim.sh
@@ -1,10 +1,4 @@
-echo "Welcome to PT-Sim, the page table simulator"
-echo "Using input file $1"
+# PT-Sim-Clock shell script
+# This script runs the page table simulator for Part B with Clock algorithm
 
-# This is where you would run your program
-
-# If it were a binary called pt-sim.x, just do
-# ./pt-sim.x $1
-# and it will read from stdin, as normal
-
-python3 ptsim.py $1
+python3 ptsim.py "$1" --clock --hex
