a8a9b666aa82b4cf985e268e13276ae3105f3e9d
mazumderr <135399797+mazumderr@users.noreply.github.com>
2025-05-02 00:49:05 -0700

    Update pt-sim.sh and test script to match project specifications

--- pt-sim.sh
+++ pt-sim.sh
@@ -1,11 +1,8 @@
 # PT-Sim shell script
 # This script runs the page table simulator for Part A
 
-if [[ "$2" == "--hex" ]]; then
-    python3 ptsim.py "$1" --hex
-else
-    python3 ptsim.py "$1"
-fi
+# Run the Python implementation
+python3 ptsim.py "$1"
 
 # If it were a binary called pt-sim.x, just do
 # ./pt-sim.x $1
